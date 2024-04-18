30f2934efd49cf7654dcc1a069fd7bba3390763a
William Amouh <williamamouh@Williams-Laptop.fritz.box>
2024-04-18 12:39:40 +0200

    add iac code to provide eks cluster and scale down main-app

---
+++ iac/terraform.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.7.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+  }
+
+  required_version = "~> 1.3"
+}
+
